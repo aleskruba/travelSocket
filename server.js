6c88e4ac44b952383aafdf3702226bf64b094775
Ales <aleskruba@seznam.cz>
2024-06-16 21:47:05 +0200

    socket uri III

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin:[ "http://localhost:3000","https://travel-cc3l.onrender.com"],
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 });
